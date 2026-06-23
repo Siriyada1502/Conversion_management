f459fc5d7a2f2f11046ceaf1561ca261ca94264f
Siriyada1502 <siriyada5575mint@gmail.com>
2026-06-23 12:57:16 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -274,8 +274,7 @@ export default function App() {
   return (
     <div className="page">
       <aside className="sidebar">
-        <h1>ระบบจัดการแปลง222</h1>
-        <p className="subtitle">React + Leaflet + JSON</p>
+        <h1>ระบบจัดการแปลง</h1>
 
         <input
           className="search"
